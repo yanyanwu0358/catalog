bfeaafe3191e55e0cf851885d652938cc4e83b59
yanyanwu0358 <wuy@ge.com>
2018-06-03 11:43:21 -0500

    Update profanity_filter.py
    bw_file_path = os.path.join(current_file_dir, "static/bad_words.txt")

--- profanity_filter.py
+++ profanity_filter.py
@@ -9,6 +9,10 @@ print word
 """
 import re
 
+current_file_path = __file__
+current_file_dir = os.path.dirname(__file__)
+bw_file_path = os.path.join(current_file_dir, "static/bad_words.txt")
+
 class Filter(object):
     """
     Replaces a bad word in a string with something more PG friendly
@@ -18,9 +22,9 @@ class Filter(object):
     """
     def __init__(self, original_string, clean_word='****'):
         
-        bad_words_file = open('./static/bad_words.txt', 'r')
+        bad_words_file = open(bw_file_path, 'r')
         
-        self.bad_words = set(line.strip('\n') for line in open('./static/bad_words.txt'))
+        self.bad_words = set(line.strip('\n') for line in open(bw_file_path))
         self.original_string = original_string
         self.clean_word = clean_word
         
