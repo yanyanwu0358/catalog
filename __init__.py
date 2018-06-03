e7f364ae683de9fc850570e47fe5c5d282092bde
yanyanwu0358 <wuy@ge.com>
2018-06-02 20:33:43 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,4 +3,4 @@ from places import app as application
 if __name__ == "__main__":
     application.secret_key = 'AKIAJ4UJCKHR4TOLPJRQ'
     application.debug = True
-    application.run(host='13.58.191.212', port=80)
+    application.run(host='18.220.11.125', port=2200)
