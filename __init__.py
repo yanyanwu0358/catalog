889ce4d92923d05e42fc48e407dcf63cb3a2312a
yanyanwu0358 <wuy@ge.com>
2018-06-02 21:30:19 -0500

    Update __init__.py
    updated the port to 80

--- __init__.py
+++ __init__.py
@@ -3,4 +3,4 @@ from places import app as application
 if __name__ == "__main__":
     application.secret_key = 'AKIAJ4UJCKHR4TOLPJRQ'
     application.debug = True
-    application.run(host='18.220.11.125', port=2200)
+    application.run(host='18.220.11.125', port=80)
