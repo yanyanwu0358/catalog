d7b02642e5d2b5802bf4338a1091e6dc1965dc76
yanyanwu0358 <wuy@ge.com>
2018-06-04 22:22:39 -0500

    Update lotsofitems.py

--- lotsofitems.py
+++ lotsofitems.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from database_setup import Category, Base, Item, User
 import datetime
 
-engine = create_engine('postgresql://catalog:password@localhost/categoryitemwithusers')
+engine = create_engine('postgresql://catalog:password@localhost/categoryitem')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
